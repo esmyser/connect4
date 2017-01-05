89cb99a9a3fdcbe5f5d31d8d56d84f579c196110
esmyser <esmyser@gmail.com>
2017-01-05 09:25:49 -0500

    four in a row wins

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -39,9 +39,137 @@ const noSpots = (row) => {
     return row !== 0 && !row;
 };
 
+const checkHorizontalLeft = (board, col, row, player) => { 
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col][row - i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkHorizontalRight = (board, col, row, player) => { 
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col][row + i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkVerticalLeft = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col - i][row] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkVerticalRight = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col + i][row] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkDiagonalTopDownLeft = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col - i][row - i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkDiagonalTopDownRight = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col + i][row + i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkDiagonalBottomDownLeft = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col + i][row - i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkDiagonalBottomDownRight = (board, col, row, player) => {
+    let found = true;
+
+    for (var i=0; i<4; i++) {
+        if (board[col - i][row + i] !== player){
+            found = false;
+            break;
+        }
+    }
+
+    return found;
+};
+
+const checkHorizontal = (board, col, row, player) => { 
+    return checkHorizontalLeft(board, col, row, player) || checkHorizontalRight(board, col, row, player);
+};
+
+const checkVertical = (board, col, row, player) => { 
+    return checkVerticalLeft(board, col, row, player) || checkVerticalRight(board, col, row, player);
+};
+
+const checkDiagonalTopDown = (board, col, row, player) => { 
+    return checkDiagonalTopDownLeft(board, col, row, player) || checkDiagonalTopDownRight(board, col, row, player);
+};
+
+const checkDiagonalBottomUp = (board, col, row, player) => { 
+    return checkDiagonalBottomDownLeft(board, col, row, player) || checkDiagonalBottomDownRight(board, col, row, player);
+};
+
 const wonGame = (board, col, row, player) => {
     // Todo: winning logic
-    return true;
+    if (checkHorizontal(board, col, row, player) || 
+        checkVertical(board, col, row, player) || 
+        checkDiagonalTopDown(board, col, row, player) || 
+        checkDiagonalBottomUp(board, col, row, player)
+    ) { 
+        return true;
+    }
+
+    return false;
 };
 
 const nextPlayer = (players, player) => {
