b314ab9d33d8b5e3022f7580e7e77722def65e1a
esmyser <esmyser@gmail.com>
2017-01-12 23:46:10 -0500

    hovering mobile with react component

--- src/containers/Column.js
+++ src/containers/Column.js
@@ -1,29 +1,73 @@
 import React from 'react';
 import Spot from './Spot';
 
-let Column = ({ dispatch, cols, rows, player, onClick  }) => {
-    let colClass = "player" + player + " col-xs-" + parseInt(12 / cols) + " column";
-    let dropClass = "col-xs-12 drop";
+class Column extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+          isHovering: false,
+        };
+    }
 
-    return (
-        <div 
-            className={ colClass }
-            onClick={ onClick }
-        >
-            <div 
-                className={ dropClass }
+    handleMouseOver () {
+        this.setState({ isHovering: true });
+    }
+  
+    handleMouseOut () {
+        this.setState({ isHovering: false });
+    }
+  
+    render () {
+        let colClass = "player" + this.props.player + " col-xs-" + parseInt(12 / this.props.cols) + " column";
+        let dropClass = "col-xs-12 drop";
+
+        return (
+            <div
+                onMouseOver={ this.handleMouseOver.bind(this) }
+                onMouseOut={ this.handleMouseOut.bind(this) }
+                className={ colClass }
+                onClick={ this.props.onClick }
             >
+                <div 
+                    className={ dropClass }
+                >
+                </div>
+                {
+                    this.props.rows.map((player, index) => 
+                        <Spot 
+                            key={ index } 
+                            player={ player }
+                        />
+                    )
+                }
             </div>
-        {
-            rows.map((player, index) => 
-                <Spot 
-                    key={ index } 
-                    player={ player }
-                />
-            )
-        }
-        </div>
-    );
+        );
+    }
 }
+
+// let Column = (props) => {
+//     let colClass = "player" + props.player + " col-xs-" + parseInt(12 / props.cols) + " column";
+//     let dropClass = "col-xs-12 drop";
+
+    // return (
+    //     <div 
+    //         className={ colClass }
+    //         onClick={ props.onClick }
+    //     >
+    //         <div 
+    //             className={ dropClass }
+    //         >
+    //         </div>
+    //         {
+    //             props.rows.map((player, index) => 
+    //                 <Spot 
+    //                     key={ index } 
+    //                     player={ player }
+    //                 />
+    //             )
+    //         }
+    //     </div>
+    // );
+// }
 
 export default Column;
